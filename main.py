83e3abd713d406b7e43859c31023930c4412efb5
Ward Toulet <ward@toulet.net>
2021-10-27 14:41:40 +0200

    Finished tutorial part 1
    Did not copy the level data

---
+++ main.py
@@ -0,0 +1,79 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 1000
+screen_height = 1000
+tile_size = 50
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Platformer')
+
+# Load images
+img_sun = pygame.image.load('img/sun.png')
+img_bkg = pygame.image.load('img/sky.png')
+
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+class World:
+    def __init__(self, data):
+        self.tile_list = []
+
+        # load images
+        img_dirt = pygame.image.load('img/dirt.png')
+        img_grass = pygame.image.load('img/grass.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(img_dirt, (tile_size, tile_size))
+                    rect = img.get_rect()
+                    rect.x = col_count * tile_size
+                    rect.y = row_count * tile_size
+                    tile = (img, rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(img_grass, (tile_size, tile_size))
+                    rect = img.get_rect()
+                    rect.x = col_count * tile_size
+                    rect.y = row_count * tile_size
+                    tile = (img, rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world_data = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 2, 2, 2, 1],
+]
+
+world = World(world_data)
+
+run = True
+while run:
+    screen.blit(img_bkg, (0, 0))
+    screen.blit(img_sun, (100, 100))
+
+    world.draw()
+    draw_grid()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
