a0327fa34c03bd630d6e96012355284dbc5fce21
Ward Toulet <ward@toulet.net>
2021-10-27 18:35:23 +0200

    Part 8

--- game.py
+++ game.py
@@ -8,8 +8,10 @@ from world import World
 from button import Button
 
 class GameState(Enum):
-    PLAYING = 1
     GAME_OVER = 0
+    START = 1
+    PLAYING = 2
+    EXIT = 3
 
 class Game:
     def __init__(self, width, height, tile_size, world, fps=60, name="Game"):
@@ -20,15 +22,23 @@ class Game:
 
         self.fps = fps
 
-        self.game_state = GameState.PLAYING
+        self.game_state = GameState.START
 
         self.player = Player(100, self.height - 130)
         self.world = world
         self.clock = pygame.time.Clock()
 
         # FIXME: move to proper location
+        # ===================================================================================================== 
         image_restart = pygame.image.load('img/restart_btn.png')
         self.restart_button = Button(self.width // 2 - 50, self.height // 2 + 100, image_restart, self.reset)
+
+        image_start = pygame.image.load('img/start_btn.png')
+        self.start_button = Button(self.width // 2 - 350, self.height // 2, image_start, self.start)
+
+        image_exit = pygame.image.load('img/exit_btn.png')
+        self.exit_button = Button(self.width // 2 + 150, self.height // 2 , image_exit, self.exit)
+        # ===================================================================================================== 
 
         pygame.display.set_caption('Platformer')
         self.screen = pygame.display.set_mode((self.width, self.height))
@@ -37,9 +47,13 @@ class Game:
         img_sun = pygame.image.load('img/sun.png')
         img_bkg = pygame.image.load('img/sky.png')
 
-        while True:
+        while self.game_state != GameState.EXIT:
             # Maintain a constant FPS
             self.clock.tick(self.fps)
+
+            # Draw background
+            self.screen.blit(img_bkg, (0, 0))
+            self.screen.blit(img_sun, (100, 100))
 
             # Run the code
             if self.game_state == GameState.PLAYING:
@@ -49,24 +63,29 @@ class Game:
                 if not is_alive:
                     self.game_state = GameState.GAME_OVER
 
+                self.world.draw(self.screen)
+                self.player.draw(self.screen)
+
+            # Show the restart menu
+            elif self.game_state == GameState.GAME_OVER:
+                self.player.update_death()
+                self.restart_button.update()
+                self.restart_button.draw(self.screen)
+
+                self.world.draw(self.screen)
+                self.player.draw(self.screen)
+
+            elif self.game_state == GameState.START:
+                self.start_button.update()
+                self.start_button.draw(self.screen)
+
+                self.exit_button.update()
+                self.exit_button.draw(self.screen)
+
             # Habdle quit event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-
-            # Draw background
-            self.screen.blit(img_bkg, (0, 0))
-            self.screen.blit(img_sun, (100, 100))
-
-            # Draw the world and the player
-            self.world.draw(self.screen)
-            self.player.draw(self.screen)
-
-            # Show the restart menu
-            if self.game_state == GameState.GAME_OVER:
-                self.player.update_death()
-                self.restart_button.update()
-                self.restart_button.draw(self.screen)
 
             # Update the display
             pygame.display.update()
@@ -74,6 +93,12 @@ class Game:
     def reset(self):
         self.game_state = GameState.PLAYING
         self.player.reset(100, self.height - 130)
+    
+    def start(self):
+        self.game_state = GameState.PLAYING
+
+    def exit(self):
+        self.game_state = GameState.EXIT
 
     def __del__(self):
         pygame.quit()
